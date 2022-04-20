5d652cc8d02b5fc6468ad67ce626c301bd12198e
aleksandrchuyko <chao.mess@gmail.com>
2022-04-20 23:57:37 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+const ingredientsItems = [];
+ingredients.forEach(ingredient => {
+  const elem = document.createElement('li');
+  elem.classList.add("item");
+  elem.textContent = ingredient;
+  ingredientsItems.push(elem);
+});
+// const ingredientsItems = ingredients.map(ingredient => {
+//   const elem = document.createElement('li');
+//   elem.classList.add("item");
+//   elem.textContent = ingredient;
+// return elem;})
+ingredientsList.append(...ingredientsItems);
+
