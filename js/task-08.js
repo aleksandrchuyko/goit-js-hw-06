0f43aa9a371001369d8cdb887e264b359a44a25b
aleksandrchuyko <chao.mess@gmail.com>
2022-05-01 00:47:42 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -6,9 +6,16 @@ const formData = {};
 
 formRef.addEventListener('submit', (e) => {
     e.preventDefault();
-    if (emailRef.value && passwordRef.value) {
-        formData[emailRef.getAttribute('name')] = emailRef.value;
-        formData[passwordRef.getAttribute('name')] = passwordRef.value;
+    // if (emailRef.value && passwordRef.value) {
+    //     formData[emailRef.getAttribute('name')] = emailRef.value;
+    //     formData[passwordRef.getAttribute('name')] = passwordRef.value;
+        
+    //     console.log(formData);
+    // }
+     if (formRef.elements[0].value && formRef.elements[1].value) {
+        formData[formRef.elements[0].name] = formRef.elements[0].value;
+        formData[formRef.elements[1].name] = formRef.elements[1].value;
+        
         console.log(formData);
     } else {
         alert("Заполните все поля!");
